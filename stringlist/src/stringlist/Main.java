29937e2cff8e27a5793b2e0aa062a0d850d70763
se-vl <se-vl@users.noreply.github.com>
2015-06-03 15:46:16 +0200

    implement Iterator

--- stringlist/src/stringlist/Main.java
+++ stringlist/src/stringlist/Main.java
@@ -1,7 +1,27 @@
 package stringlist;
 
+import java.util.Iterator;
+
 public class Main
 {
+    public static void oldschool(StringList scientists)
+    {
+        Iterator<String> it = scientists.iterator();
+        while (it.hasNext())
+        {
+            String scientist = it.next();
+            System.out.println(scientist);
+        }
+    }
+
+    public static void newschool(StringList scientists)
+    {
+        for (String scientist : scientists)
+        {
+            System.out.println(scientist);
+        }
+    }
+
     public static void main(String[] args)
     {
         StringList scientists = new StringList();
@@ -12,9 +32,7 @@ public class Main
         scientists.add("Feynman");
         scientists.add("Oppenheimer");
 
-        for (int i = 0; i < scientists.length(); ++i)
-        {
-            System.out.println(scientists.get(i));
-        }
+        //        oldschool(scientists);
+        newschool(scientists);
     }
 }
--- stringlist/src/stringlist/StringList.java
+++ stringlist/src/stringlist/StringList.java
@@ -1,11 +1,41 @@
 package stringlist;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
-public class StringList
+public class StringList implements Iterable<String>
 {
     private String[] _array = new String[10];
     private int _length;
+
+    @Override
+    public Iterator<String> iterator()
+    {
+        return new StringListIterator();
+    }
+
+    private class StringListIterator implements Iterator<String>
+    {
+        private int _index = 0;
+
+        @Override
+        public boolean hasNext()
+        {
+            return _index < _length;
+        }
+
+        @Override
+        public String next()
+        {
+            return _array[_index++];
+        }
+
+        @Override
+        public void remove()
+        {
+            throw new UnsupportedOperationException();
+        }
+    }
 
     public int length()
     {
